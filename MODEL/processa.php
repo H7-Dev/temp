bd3720aa8230af8d51dcccb92884fb29a8962a95
Herbert <herbert.dev.alfa@gmail.com>
2023-04-28 15:14:01 -0300

    01.01 [-g-s] 👍 XMLHttpRequest @amostra 001

--- MODEL/processa.php
+++ MODEL/processa.php
@@ -1,12 +1,9 @@
 <?php
-
     $termo1 = $_GET["t1"];
     $termo2 = $_GET["t2"];
-
     $resposta = $termo1 . " -- " . $termo2;
-    // print $resposta;
-    echo "<script>alert('O arquivo processa.php foi chamado.')</script>";
-    echo $resposta;
-
-
+    $alerta = "O arquivo processa.php foi chamado.";
+    $resultado = array('alerta' => $alerta, 'resposta' => $resposta);
+    header('Content-Type: application/json');
+    echo json_encode($resultado);
 ?>
--- VIEW/JS/ajax.js
+++ VIEW/JS/ajax.js
@@ -1,10 +1,15 @@
+
 function processa() {
     var t1 = document.getElementById("termo1").value;
     var t2 = document.getElementById("termo2").value;
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
-        document.getElementById("resposta").innerHTML = this.responseText;
+        var respostaJSON = JSON.parse(this.responseText);
+        var alerta = respostaJSON.alerta;
+        var resposta = respostaJSON.resposta;
+        alert(alerta);
+        document.getElementById("resposta").innerHTML = resposta;
     }
     xhttp.open("GET", "./MODEL/processa.php?t1="+t1+"&t2="+t2);
     xhttp.send();    
-}      
+}
