60fba2ebffaadf9f5204e7a10f9a87d14cd42d19
Herbert <herbert.dev.alfa@gmail.com>
2023-04-29 06:31:20 -0300

    01.07-B [-g-s] controller e model prontos

--- CONTROLLER/atualizar_favorito.php
+++ CONTROLLER/atualizar_favorito.php
@@ -12,20 +12,17 @@
     
         public function atualizarFavorito($dados) {
             try {
-                $this->baralhosDAO->atualizarFavorito($dados['idBar'], $dados['novoValor']);
+                $this->baralhosDAO->atualizarFavorito($dados['novoValor'],$dados['idBar']);
                 echo json_encode(['success' => true, 'message' => 'Favorito atualizado com sucesso.']);
             } catch (Exception $e) {
                 echo json_encode(['success' => false, 'message' => 'Erro ao atualizar favorito: ' . $e->getMessage()]);
             }
         }
-    
     }
-
     $dados = [
         'idBar' => 2,
-        'novoValor' => 1
+        'novoValor' => 0
     ];
-    
     $favoritoController = new FavoritoController();
     $favoritoController->atualizarFavorito($dados);
 
