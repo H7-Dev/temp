7c31757734fb2e9c89ed35f50676f77220b2401e
Herbert <herbert.dev.alfa@gmail.com>
2023-04-29 08:41:37 -0300

    01.10 [-g-s] ❗❗❗ tratar class FavoritoController e solicitação ajax (XMLHttpRequest)

--- CONTROLLER/atualizar_favorito.php
+++ CONTROLLER/atualizar_favorito.php
@@ -11,30 +11,35 @@
         }
     
         public function atualizarBaralhoFavorito($idBar, $novoValor) {
-    
+
             // Verificação se o baralho com o ID fornecido existe na tabela
             $baralho = $this->baralhosDAO->getBaralhoById($idBar);
             if (!$baralho) {
                 echo json_encode(['success' => false, 'message' => 'Baralho não encontrado.'], JSON_UNESCAPED_UNICODE);
                 return;
             }
-    
+        
             // Verificação se o novo valor é um caractere numérico dentro do intervalo permitido
             if (!is_numeric($novoValor) || $novoValor < 0 || $novoValor > 1) {
                 echo json_encode(['success' => false, 'message' => 'Novo valor inválido. O favorito deve ser um número de 0 a 1.'], JSON_UNESCAPED_UNICODE);
                 return;
             }
-    
+        
             // Execução da atualização
             try {
-                $this->baralhosDAO->atualizarCampoFavorito($novoValor, $idBar);
-                echo json_encode(['success' => true, 'message' => 'Favorito atualizado com sucesso.'], JSON_UNESCAPED_UNICODE);
-                return; 
+                $resultado = $this->baralhosDAO->atualizarCampoFavorito($novoValor, $idBar);
+                if ($resultado) { // se o método baralhosDAO->atualizarCampoFavorito retorna true
+                    echo json_encode(['success' => true, 'message' => 'Favorito atualizado com sucesso.'], JSON_UNESCAPED_UNICODE);
+                } else { // se o método baralhosDAO->atualizarCampoFavorito retorna false
+                    echo json_encode(['success' => false, 'message' => 'Erro ao atualizar favorito.'], JSON_UNESCAPED_UNICODE);
+                }
+                return;
             } catch (Exception $e) {
                 echo json_encode(['success' => false, 'message' => 'Erro ao atualizar favorito: ' . $e->getMessage()], JSON_UNESCAPED_UNICODE);
                 return ;
             }
         }
+        
     }
     
     
@@ -45,6 +50,4 @@
         $favoritoController = new FavoritoController();
         $favoritoController->atualizarBaralhoFavorito($idBar, $novoValor);
     }
-    
-
     ?>
